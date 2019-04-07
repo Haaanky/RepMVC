3a2bbbd539503f84b2bb9ce90cb17bbdcb119e0d
Henrik NF <Henriknorfri@gmail.com>
2019-04-07 19:28:29 +0200

    lite kommentarer bara

--- RepQuiz/Controllers/HomeController.cs
+++ RepQuiz/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// Behövs typ inte
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
--- RepQuiz/Models/Band.cs
+++ RepQuiz/Models/Band.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Uppgift 1
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
